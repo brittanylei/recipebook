89eebbf6bfa6e2e730d815c6aeb643b485c9b317
brittanylei <brittlei48@gmail.com>
2020-06-16 14:02:06 -0700

    fixed urls

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:recipe_id>/', views.detail, name='detail'),
-    path('addRecipe/', views.add_recipe, name='addrecipe'),
-    path('<int:recipe_id>/deleteRecipe/', views.delete_recipe, name='delete'),
-    path('<int:recipe_id>/editRecipe/', views.edit_recipe, name='edit'),
-    path('addRecipe/addIngredient/', views.add_ingredient, name='addingredient'),
+    path('addRecipe/', views.add_recipe, name='add-recipe'),
+    path('<int:recipe_id>/deleteRecipe/', views.delete_recipe, name='delete-recipe'),
+    path('<int:recipe_id>/editRecipe/', views.edit_recipe, name='edit-recipe'),
+    path('addRecipe/addIngredient/', views.add_ingredient, name='add-ingredient'),
 ]
