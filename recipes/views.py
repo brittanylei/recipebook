b5785cecb809ca0a65b9e3bfdba2c25146ba642a
brittanylei <brittlei48@gmail.com>
2020-06-17 16:32:19 -0700

    added login decorators

--- recipes/views.py
+++ recipes/views.py
@@ -7,10 +7,12 @@ from .forms import RecipeForm, IngredientForm, DirectionForm, UnitForm
 from django.forms import modelformset_factory, formset_factory
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required(login_url='accounts:login')
 def index(request, category_name=''):
     if category_name:
         recipes = Recipe.objects.filter(category__name=category_name, user=request.user)
@@ -21,12 +23,14 @@ def index(request, category_name=''):
     return render(request, 'index.html', context)
 
 
+@login_required(login_url='accounts:login')
 def detail(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     context = {'recipe': recipe}
     return render(request, 'detail.html', context)
 
 
+@login_required(login_url='accounts:login')
 def add_recipe(request):
     form = RecipeForm(request.POST or None)
     # d_set = modelformset_factory(model=Direction, form=DirectionForm, extra=3, exclude=())
@@ -53,6 +57,7 @@ def add_recipe(request):
     return render(request, 'addRecipe.html', context)
 
 
+@login_required(login_url='accounts:login')
 def delete_recipe(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     if request.method == "POST":
@@ -62,6 +67,7 @@ def delete_recipe(request, recipe_id):
     return render(request, 'deleteRecipe.html', context)
 
 
+@login_required(login_url='accounts:login')
 def edit_recipe(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     form = RecipeForm(request.POST or None, instance=recipe)
@@ -72,6 +78,7 @@ def edit_recipe(request, recipe_id):
     return render(request, 'editRecipe.html', context)
 
 
+@login_required(login_url='accounts:login')
 def edit_add_ingredient(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     form = IngredientForm(request.POST or None)
@@ -82,6 +89,7 @@ def edit_add_ingredient(request, recipe_id):
     return render(request, 'addIngredient.html', context)
 
 
+@login_required(login_url='accounts:login')
 def add_ingredient(request):
     form = IngredientForm(request.POST or None)
     if form.is_valid():
@@ -91,6 +99,7 @@ def add_ingredient(request):
     return render(request, 'addIngredient.html', context)
 
 
+@login_required(login_url='accounts:login')
 def delete_ingredient(request, ingredient_id):
     ingredient = Ingredient.objects.get(id=ingredient_id)
     if request.method == "POST":
