1d4e56095f1aa603d02b242e8fd9a480af8967de
brittanylei <brittlei48@gmail.com>
2020-06-18 15:08:14 -0700

    add/edit ingredient view formatting

--- recipes/forms.py
+++ recipes/forms.py
@@ -19,9 +19,21 @@ class RecipeForm(forms.ModelForm):
             'notes': forms.Textarea(attrs={'rows': 5, 'cols': 50}),
         }
 
-    # def __init__(self, *args, **kwargs):
-    #     self.user = kwargs.pop('user')
-    #     super(RecipeForm, self).__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        #     self.user = kwargs.pop('user')
+        #     super(RecipeForm, self).__init__(*args, **kwargs)
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
+        self.fields['ingredients'].widget.attrs.update({
+            'class': 'form-check-input',
+            # 'class': 'form-inline'
+        })
+        self.fields['category'].widget.attrs.update({
+            'class': 'form-check-input'
+        })
 
 
 class IngredientForm(forms.ModelForm):
@@ -31,11 +43,24 @@ class IngredientForm(forms.ModelForm):
             'name', 'amount', 'unit'
         ]
 
+    def __init__(self, *args, **kwargs):
+        super(IngredientForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
 
 class UnitForm(forms.ModelForm):
     class Meta:
         model = Unit
         fields = ['measure']
+
+    def __init__(self, *args, **kwargs):
+        super(UnitForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
 
 
 class DirectionForm(forms.ModelForm):
@@ -45,4 +70,11 @@ class DirectionForm(forms.ModelForm):
             'step', 'description', 'recipe'
         ]
         exclude = ('recipe',)
+
+    def __init__(self, *args, **kwargs):
+        super(DirectionForm, self).__init__(*args, **kwargs)
+        for field in iter(self.fields):
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
 
